57f72b6eda29230594314ec205833c5de8d4f208
Scott Kushnier <kushnier@hstreet.com>
2023-10-31 19:12:06 -0400

    some comments

--- app.py
+++ app.py
@@ -18,27 +18,30 @@ def home_page():
     return(render_template('home.html', title=survey.title))
 
 @app.route('/questions/<int:q_ind>')
+""" ask question number 'q_ind'+1 """
 def question_n(q_ind):
-    if (len(responses) >= num_questions):
+    if (len(responses) >= num_questions):   # got enough responses, thanks!
         return redirect("/thankyou", code=302)
-    elif (q_ind != len(responses)):
+    elif (q_ind != len(responses)):       # somehow got on wrong page, redirect w/msg
         flash("(You've been redirected to proper next question)");
         return redirect(f"/questions/{len(responses)}", code=302)
-    else:
+    else:   
         return render_template('question.html', title=survey.title, question_num=q_ind+1, question=survey.questions[q_ind].question,
                             choices=survey.questions[q_ind].choices)
 
 @app.route('/answer', methods=['POST'])
+""" POST route for answer given, add to responses & redirect to next question """
 def answer():
     responses.append(request.form['answer'])
  #   print(request.form['answer'])
     q_ind = len(responses)
-    print(responses, q_ind, num_questions)
+ #   print(responses, q_ind, num_questions)
     if (q_ind < num_questions):
         return redirect(f"/questions/{q_ind}", code=302)
     else:
         return redirect("/thankyou", code=302)
 
 @app.route('/thankyou')
+""" All done, put up thanks page """
 def thankyou():
     return(render_template('thankyou.html', title=survey.title))
